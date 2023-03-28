07828275bac5557b7cfb81c7ac86a82161c3c1a1
singhxayush <fanduayush2000@gmail.com>
2023-03-29 03:45:09 +0530

    Made it 10x SEXIER added stage and unstage feature & separate stage and specific commit feature ;)

--- commit.sh
+++ commit.sh
@@ -131,5 +131,5 @@ DESCRIPTION=$(gum write --placeholder "Enter update message... (CTRL+D to finish
 gum confirm "Push?" && ((echo $tracked_files | git commit -m "$DESCRIPTION") && git push)
 
 
-reset_screen
-gum style --border rounded --border-foreground="#33cc33" --bold --width=1 --padding="0 1" "DONE!";
+clear
+gum style --border rounded --border-foreground="#33cc33" --bold --width=1 --padding="0 3" "DONE!";
