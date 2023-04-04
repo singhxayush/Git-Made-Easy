415287908acbcd52c577b14e8a3fd08eb4f3316b
singhxayush <fanduayush2000@gmail.com>
2023-04-05 03:33:44 +0530

    Initiating Unstage feature, 1st commit regarding the same. Basic banner created so far only!

---
+++ unstage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#! CLEARS THE SCREEN BEFORE STARING - UNCOMMENT THIS IF YOU DON'T LIKE
+clear
+
+
+
+#################! STYLING AND DECLARATIONS #################
+print_banner() {
+    gum style \
+    --border rounded  \
+    --border-foreground="#66b3ff" \
+    --bold \
+    --width=3 \
+    --padding="0 4" \
+    "Unstage Your Changes 💬"
+}
+print_banner
+
